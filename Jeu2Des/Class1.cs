4a1887c85ce9120feb63e3eaf1e35917eceae560
cdi8dorm <cdi8dorm@M2201>
2016-09-12 13:45:25 +0200

    Ajoutez des fichiers projet.

---
+++ Jeu2Des/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jeu2Des
+{
+	/// <summary>
+    /// 
+    /// </summary>
+    public class Class1
+    {
+		#region Attributes and properties
+		
+		#endregion
+		
+		#region Constructors
+		
+		#endregion
+		
+		#region Method
+		
+		#endregion
+    }
+}
